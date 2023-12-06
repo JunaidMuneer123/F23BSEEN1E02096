6bb1b2d6a23175e8ecaf9728a0841527edaf2833
JunaidMuneer123 <150917304+JunaidMuneer123@users.noreply.github.com>
2023-12-06 00:39:59 -0800

    Update Exercise 2.py

--- Exercise 2.py
+++ Exercise 2.py
@@ -1,7 +1,11 @@
-# Get input from the user
-user_input = input("Enter a string: ")
+print("printing current and previous number and theirsum in range(10)")
 
-# Iterate through the string and print characters at even indices
-print("Characters at even indices:")
-for index in range(0, len(user_input), 2):
-    print(user_input[index])
+previous_num = 0
+#Loop from 1 to 10:
+for i in range(1,11):
+    x_sum = previous_num + i
+    print("current number", i , "previous number", previous_num, "sum:", x_sum)
+    # modify previous number
+    #set it to the current number
+    previous_num = i
+
